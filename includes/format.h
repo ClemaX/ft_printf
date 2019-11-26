f251287cc1b98cb4995245ddcfec3405c325219a
Clément Hamada <clementhamada@protonmail.com>
2019-11-26 06:11:52 +0100

    Make f_format extern in header

--- includes/format.h
+++ includes/format.h
@@ -6,7 +6,7 @@
 /*   By: chamada <chamada@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/21 22:02:48 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/24 21:55:20 by chamada     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/26 06:08:46 by chamada     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -14,6 +14,6 @@
 #ifndef FORMAT_H
 # define FORMAT_H
 
-t_line	*(*g_format[9])(t_line **line, t_spec spec, va_list ap);
+extern t_line	*(*g_format[10])(t_line **line, t_spec spec, va_list ap);
 
 #endif
